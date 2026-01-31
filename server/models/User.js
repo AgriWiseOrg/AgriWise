ceca356c6a1034c4ad9dbdaada679edf753fc8be
Ananthasagar <ananthasagarn13@gmail.com>
2026-01-31 12:46:14 +0530

    different roles

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  phoneNumber: {
+  email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
   },
   role: {
     type: String,
+    enum: ['farmer', 'buyer', 'admin'],
     default: 'farmer'
   }
 }, { timestamps: true });
